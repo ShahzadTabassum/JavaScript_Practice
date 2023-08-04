30c1ff3a768aafa2e7e67352fcc8bf4b5caf9f48
ShahzadTabassum <shahzadt481@gmail.com>
2023-08-04 22:53:46 +0500

    Today Update.

--- First Alert/index.js
+++ First Alert/index.js
@@ -6,3 +6,9 @@ console.log(35);
 
 document.write("Shahzad");
 document.write(4+9);
+
+var name_type = "Alart! Don't Enter Wrong Information";
+
+alert(name_type);
+alert(name_type.toLowerCase());
+alert(name_type.toUpperCase());
